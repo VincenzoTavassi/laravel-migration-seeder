85466a1b822460826243d016dd5b5a6437506251
VincenzoTavassi <info@egadivacanze.it>
2023-03-30 16:21:42 +0200

    carbon today trains

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Train;
+use Carbon\Carbon;
 
 class PageController extends Controller
 {
     public function index()
     {
-        $trains = Train::all();
+        $today = Carbon::today();
+        $trains = Train::whereDate('departure', $today)->get();;
         return view('homepage', compact('trains'));
     }
 }
